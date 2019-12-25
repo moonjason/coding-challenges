a75b310419c4ccc7aea84db8dc0a8df34cb7e74c
Jason Moon <jasonmoonjm@gmail.com>
2019-12-25 12:59:30 -0800

    day 1 part 1

---
+++ leetCode/sudokuSolver37.js
@@ -0,0 +1,26 @@
+// Leet Code 37 - Sudoku Solver
+//  https://leetcode.com/problems/sudoku-solver/
+
+// 2D Array 3x3 Sub-Boxes 
+// Each 
+
+const board = [
+    [5, 3, '.', '.', 7, '.', '.', '.', '.'],
+    [6, '.', '.', 1, 9, 5, '.', '.', '.'],
+    ['.', 9, 8, '.', '.', '.', '.', 6, '.'],
+    [8, '.', '.', '.', 6, '.', '.', '.', 3],
+    [4, '.', '.', 8, '.', 3, '.', '.', 1],
+    [7, '.', '.', '.', 2, '.', '.', '.', 6],
+    ['.', 6, '.', '.', '.', '.', 2, 8, '.'],
+    ['.', '.', '.', 4, 1, 9, '.', '.', 5],
+    ['.', '.', '.', '.', 8, '.', '.', 7, 9],
+];
+
+const solveSudoku = () => { 
+
+}; 
+ 
+board.forEach(arr => {
+    console.log(arr.length)
+})
+console.log(board.length, 'boardlength')
