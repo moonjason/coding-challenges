0c3561b83c70d527afe934483f03e4fa4e76a69a
Jason Moon <jasonmoonjm@gmail.com>
2019-12-24 18:14:36 -0800

    day 0 done

--- 10daysOfJavaScript/day0.js
+++ 10daysOfJavaScript/day0.js
@@ -1,3 +1,5 @@
+// Problem 1 
+
 /**
 *   A line of code that prints "Hello, World!" on a new line is provided in the editor. 
 *   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
@@ -18,3 +20,40 @@ function greeting(parameterVariable) {
 
 
 greeting('I love JavaScript');
+
+// Problem 2 
+
+/**
+*   The variables 'firstInteger', 'firstDecimal', and 'firstString' are declared for you -- do not modify them.
+*   Print three lines:
+*   1. The sum of 'firstInteger' and the Number representation of 'secondInteger'.
+*   2. The sum of 'firstDecimal' and the Number representation of 'secondDecimal'.
+*   3. The concatenation of 'firstString' and 'secondString' ('firstString' must be first).
+*
+*	Parameter(s):
+*   secondInteger - The string representation of an integer.
+*   secondDecimal - The string representation of a floating-point number.
+*   secondString - A string consisting of one or more space-separated words.
+**/
+function performOperation(secondInteger, secondDecimal, secondString) {
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+    
+    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+    const firstDecimal = 4.0;
+    
+    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+    const firstString = 'HackerRank';
+    
+    // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
+    
+    console.log(firstInteger + secondInteger, '<-- First Integer + Second Integer')
+    // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
+    
+    console.log(firstDecimal + secondDecimal, '<--- First Decimal + Second Decimal')
+    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+    console.log(`${firstString} ${secondString}`)
+}
+
+
+performOperation(6, 6.2, 'is the best resource for learning code.')
