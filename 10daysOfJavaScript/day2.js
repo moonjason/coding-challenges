8a72246f1ae8aea06b3df66d4cf1c326e5f222a2
Jason Moon <jasonmoonjm@gmail.com>
2019-12-27 12:29:17 -0800

    getgrade day 2

--- 10daysOfJavaScript/day2.js
+++ 10daysOfJavaScript/day2.js
@@ -18,3 +18,25 @@ function vowelsAndConsonants(s) {
 }
 
 vowelsAndConsonants('javascriptloops');
+
+
+// #2 If - Else Statements
+// Complete the getGrade(score) function in the editor. It has one parameter: an integer, , denoting the number of points Julia earned on an exam. It must return the letter corresponding to her  according to the following rules:
+function getGrade(score) {
+    let grade;
+    // Write your code here
+    if (score <= 30 && score > 25) {
+        grade = 'A'
+    } else if (score <= 25 && score > 20) {
+        grade = 'B'
+    } else if (score <= 20 && score > 15) {
+        grade = 'C'
+    } else if (score <= 15 && score > 10) {
+        grade = 'D'
+    } else if (score <= 10 && score > 5) {
+        grade = 'E'
+    } else { 
+        grade = 'F'
+    }
+    return grade;
+}
