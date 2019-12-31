77d5a281bddd8fded52db5b8d0f29a0a7943b592
Jason Moon <jasonmoonjm@gmail.com>
2019-12-30 19:00:42 -0800

    class day 4 polygon

--- 10daysOfJavaScript/day4.js
+++ 10daysOfJavaScript/day4.js
@@ -30,3 +30,20 @@ function getCount(objects) {
     })
     return counter;
 }
+
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+
+class Polygon { 
+    constructor(sides) {
+        this.sides = sides;
+    }
+    perimeter(){
+        let result = 0;
+        this.sides.forEach(side => result += side)
+        return result
+    }
+}
