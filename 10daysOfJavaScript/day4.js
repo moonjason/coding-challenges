f40f48dde9c582e08528b7c638432ef01f04a71d
Jason Moon <jasonmoonjm@gmail.com>
2019-12-30 18:17:16 -0800

    rectangle object

---
+++ 10daysOfJavaScript/day4.js
@@ -0,0 +1,17 @@
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    const rec = {
+        length: a, 
+        width: b,
+        perimeter: 2 * (a + b),
+        area: a * b
+    }
+    console.log(rec.length);
+    console.log(rec.width);
+    console.log(rec.perimeter);
+    console.log(rec.area)
+    return result;
+}
+
