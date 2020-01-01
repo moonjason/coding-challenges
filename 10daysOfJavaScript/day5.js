38dc2060910fddfa382a3fe05bdbad21b628069b
Jason Moon <jasonmoonjm@gmail.com>
2019-12-31 23:56:56 -0800

    day5 literals???

--- 10daysOfJavaScript/day5.js
+++ 10daysOfJavaScript/day5.js
@@ -1,3 +1,4 @@
+// Inheritance 
 class Rectangle {
     constructor(w, h) {
         this.w = w;
@@ -30,3 +31,29 @@ if (JSON.stringify(Object.getOwnPropertyNames(Square.prototype)) === JSON.string
     console.log(-1);
     console.log(-1);
 }
+
+// Template Literals
+// WARNING HAS NOTHING TO DO WITH LITERALS...
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+    let A = expressions[0];
+    let P = expressions[1];
+    const answers = [];
+    const s1 = (P+Math.sqrt(Math.pow(P, 2)-16*A))/4;
+    const s2 = (P-Math.sqrt(Math.pow(P, 2)-16*A))/4;
+    
+    answers.push(s1);
+    answers.push(s2);
+    
+    return answers.sort();
+}
+
+
