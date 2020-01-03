129d84e3fbb18b72615a995c7e2ff97c6a4e963d
Jason Moon <jasonmoonjm@gmail.com>
2020-01-02 20:50:19 -0800

    day 6 date strings

--- 10daysOfJavaScript/day6.js
+++ 10daysOfJavaScript/day6.js
@@ -11,3 +11,32 @@ function getMaxLessThanK(n, k) {
     }
     return max_anb;
 }
+
+// Javascript dates
+// Given a date string, , in the format MM/DD/YYYY, find and return the day name for that date. Each day name must be one of the following strings: Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, or Saturday. For example, the day name for the date 12/07/2016 is Wednesday.
+function getDayName(dateString) {
+    let dayName = new Date(dateString).getDay();
+    switch (dayName){
+        case 0:
+            return 'Sunday';
+            break;
+        case 1:
+            return 'Monday';
+            break;
+        case 2:
+            return 'Tuesday';
+            break;
+        case 3:
+            return 'Wednesday';
+            break;
+        case 4:
+            return 'Thursday';
+            break;
+        case 5: 
+            return 'Friday';
+            break;
+        default:
+            return 'Saturday';
+    }
+    // Write your code here
+}
