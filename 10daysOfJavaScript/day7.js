3109815972661db3a5f61fdb043c967d3910192b
Jason Moon <jasonmoonjm@gmail.com>
2020-01-03 18:20:54 -0800

    regex pt2 day 7

--- 10daysOfJavaScript/day7.js
+++ 10daysOfJavaScript/day7.js
@@ -21,4 +21,19 @@ function vowelChecker(str) {
     
         let re = /^([aeiou]).+\1$/;
         return re;
-    }
+}
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+     * followed by one or more letters.
+     */
+    const re = /^(Mr|Mrs|Ms|Dr|Er)(\.)([a-zA-Z])*$/
+    // (\.)  to match a period (special character: needs to be escaped by a \)
+    // $ means the string must end with the character expression just before it. In this case it must end with an [A-za-z] character and not anything else.
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
