3572c79d742e410f602f611cf922fbec6d084c13
moonjason <jasonmoonjm@gmail.com>
2020-05-11 15:54:53 -0700

    refined version

--- leetCode/containerWithMostWater11.js
+++ leetCode/containerWithMostWater11.js
@@ -36,4 +36,23 @@ const maxArea = (height) => {
     return largestArea;
 }
 
+// var maxArea = function(height) {
+//     let i = 0;
+//     let j = height.length - 1;
+//     let max = -Infinity;
+    
+//     while(i < j) {
+//         const min = Math.min(height[i], height[j]);
+//         max = Math.max(max, min * (j - i));
+//         if (height[i] < height[j]) {
+//             i++;
+//         } else {
+//             j--;
+//         }
+//     }
+    
+//     return max;
+// };
+
+
 console.log(maxArea([1,8,6,2,5,4,8,3,7]));
