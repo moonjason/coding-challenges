c3b903978823c19b14bf76408bd60fdb5e0511f4
moonjason <jasonmoonjm@gmail.com>
2020-05-13 12:32:56 -0700

    rotate 2d array leetcode48, finished, notes

--- leetCode/rotateImage48.js
+++ leetCode/rotateImage48.js
@@ -17,3 +17,44 @@
 //   [8,5,2],
 //   [9,6,3]
 // ]
+
+
+const input = 
+                [
+                    [1,2,3],
+                    [4,5,6],
+                    [7,8,9]
+                ];
+
+const rotate = (matrix) => {
+
+
+    for (let c = 0; c < matrix.length; c++) {
+        for(let r = c; r < matrix[c].length; r++) {
+            [matrix[c][r], matrix[r][c]] = [matrix[r][c], matrix[c][r]]; 
+        }
+    }   
+
+    for (let c = 0; c < matrix.length; c++) {
+        for (let r = 0; r < matrix[c].length / 2; r++) {
+            let temp = matrix[c][r]
+            matrix[c][r] = matrix[c][matrix[c].length - 1 - r];
+            matrix[c][matrix[c].length - 1 - r] = temp;
+        }
+
+    }
+
+    return matrix;
+}
+
+console.log(rotate(input));
+
+
+
+// --  REVIEWING THIS NESTED LOOP  -- 
+
+// for (let c = 0; c < matrix.length; c++) {
+//     for(let r = c; r < matrix[c].length; r++) {
+//         [matrix[c][r], matrix[r][c]] = [matrix[r][c], matrix[c][r]]; 
+//     }
+// }   
