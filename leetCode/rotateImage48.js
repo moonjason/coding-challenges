11c8892e6beb9c9e2ee3aba65712077e06955ab0
moonjason <jasonmoonjm@gmail.com>
2020-05-13 12:01:19 -0700

    rotateImage set up problem

---
+++ leetCode/rotateImage48.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/rotate-image/
+
+// Arrays 
+
+// Given a 2D Matrix, rotate the image by 90 degrees (clockwise);
+
+// Given input matrix = 
+// [
+//   [1,2,3],
+//   [4,5,6],
+//   [7,8,9]
+// ],
+
+// rotate the input matrix in-place such that it becomes:
+// [
+//   [7,4,1],
+//   [8,5,2],
+//   [9,6,3]
+// ]
