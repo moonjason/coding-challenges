d9992ddacc08de110b187baac1f4fa86e2a02f5a
moonjason <jasonmoonjm@gmail.com>
2020-05-13 15:05:19 -0700

    rotate image notes

--- leetCode/rotateImage48.js
+++ leetCode/rotateImage48.js
@@ -30,14 +30,14 @@ const rotate = (matrix) => {
 
 
     for (let c = 0; c < matrix.length; c++) {
-        for(let r = c; r < matrix[c].length; r++) {
-            [matrix[c][r], matrix[r][c]] = [matrix[r][c], matrix[c][r]]; 
+        for(let r = c; r < matrix[c].length; r++) { 
+            [matrix[c][r], matrix[r][c]] = [matrix[r][c], matrix[c][r]]; //ES2015 SWAPPING
         }
     }   
 
     for (let c = 0; c < matrix.length; c++) {
-        for (let r = 0; r < matrix[c].length / 2; r++) {
-            let temp = matrix[c][r]
+        for (let r = 0; r < matrix[c].length / 2; r++) { // ONLY LOOPING UNTIL THE HALF OF THE ARRAY 
+            let temp = matrix[c][r]  // OTHER SWAPPING METHOD, WE ARE REVERSING THE INDICES EXCEPT THE MIDDLE COULD ALSO USE ARRAY.REVERSE()
             matrix[c][r] = matrix[c][matrix[c].length - 1 - r];
             matrix[c][matrix[c].length - 1 - r] = temp;
         }
@@ -58,3 +58,42 @@ console.log(rotate(input));
 //         [matrix[c][r], matrix[r][c]] = [matrix[r][c], matrix[c][r]]; 
 //     }
 // }   
+
+// EXPLANATION:
+// What this formula is doing is that we are swapping out each column for each row
+// if c = 0, r = 0;
+// [matrix[0][0], matrix[0][0]] = [matrix[0][0], matrix[0][0]]; 
+
+// if c = 0, r = 1;
+// [matrix[0][1], matrix[1][0]] = [matrix[1][0], matrix[0][1]]; 
+
+// [
+//   [1,2,3],
+//   [4,5,6],
+//   [7,8,9]
+// ],
+
+// = > 
+
+// [
+//   [1,4,3],
+//   [2,5,6],
+//   [7,8,9]
+// ],
+
+// if c = 0, r = 2;
+// [matrix[0][2], matrix[2][0]] = [matrix[2][0], matrix[0][2]]; 
+
+// [
+//   [1,2,3],
+//   [4,5,6],
+//   [7,8,9]
+// ],
+
+// = > 
+
+// [
+//   [1,4,3],
+//   [2,5,6],
+//   [7,8,9]
+// ],
