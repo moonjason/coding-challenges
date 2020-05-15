96dd1a7737f46bd282a0e9c4f850ebe3dd8d8ce4
moonjason <jasonmoonjm@gmail.com>
2020-05-15 14:33:01 -0700

    kids with candies Leetcode Easy

---
+++ leetCode/kidsWithCandies1431.js
@@ -0,0 +1,32 @@
+// /**
+//  * @param {number[]} candies
+//  * @param {number} extraCandies
+//  * @return {boolean[]}
+//  */
+
+var kidsWithCandies = function(candies, extraCandies) {
+    const res = [];
+    let largest = 0;
+    for (let i = 0; i < candies.length; i++) {
+        if (largest < candies[i]) {
+            largest = candies[i]
+        }
+    }
+    for ( let i = 0; i < candies.length; i++) {
+        console.log(candies[i] + extraCandies)
+        if (candies[i] + extraCandies >= largest) {
+            res.push(true);
+        } else {
+            res.push(false);
+        }
+    }
+    
+    return res;
+};
+
+console.log(kidsWithCandies([2,3,5,1,3], 3));
+
+
+// using 
+//  var max = Math.max(...candies); 
+// is a good way to find the largest number ... same run time tho i believe
